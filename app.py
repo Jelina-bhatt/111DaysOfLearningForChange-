f39e73cb9da1f63883510290aa84b09bcf8d218b
Jelina Bhatt <jelinabhatt1@gmail.com>
2026-04-15 22:45:40 +0545

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 #read data from csv file into a dataframe
-df = pd.read_csv("sales_data_sample.csv", encoding="latin1")
+#df = pd.read_csv("sales_data_sample.csv", encoding="latin1")
 
+#df=pd.read_json("sample_Data.json")
+
+df=pd.read_excel("SampleSuperstore.xlsx")
 print(df)
