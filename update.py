a127781803a08751277194f265a7e98c01bd3436
Jelina Bhatt <jelinabhatt1@gmail.com>
2026-04-24 22:29:55 +0545

    git push origin main

---
+++ update.py
@@ -0,0 +1,14 @@
+import pandas as pd
+data ={
+    "Name":['rubina','amit', 'saroj','bhawana','bindu','apekshya','binod','ashika'],
+    "Age":[30,25,40,60,28,50,33,22],
+    "Salary":[50000, 20000,30000,70000,100000,60000,400000,45000],
+    "Performance score":[79,80,45,34,56,32,88,76]
+}
+
+df=pd.DataFrame(data)
+print(df)
+
+#updating salary of rubina
+df.loc[0,"Salary"] = 60000
+print(df)
